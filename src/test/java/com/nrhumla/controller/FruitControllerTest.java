dcb7f6a1682eaa0edd35e77e47d7c9b2e1ec7e73
dbouclier@openwt.com <dbouclier@openwt.com>
2016-08-18 16:26:42 +0200

    remove unnecessary code

--- src/test/java/com/nrhumla/controller/FruitControllerTest.java
+++ src/test/java/com/nrhumla/controller/FruitControllerTest.java
@@ -12,17 +12,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.nrhumla.WebMvcConfig;
 import com.nrhumla.mock.MockFruitService;
 import com.nrhumla.service.FruitService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(FruitController.class)
-@Import(WebMvcConfig.class)
 public class FruitControllerTest
 {
     @Autowired
